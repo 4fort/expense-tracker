2144ad434afafc12614ea515779d7038f2d6d7d9
4Fort <dexterfortbsilva@gmail.com>
2025-01-22 18:56:11 +0800

    Add animation to Main component and create Badge component

--- components/main.tsx
+++ components/main.tsx
@@ -1,5 +1,18 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import React from "react";
+import { motion } from "motion/react";
+import { usePathname } from "next/navigation";
+
+const includedPaths = [
+  "/login",
+  "/signup",
+  "/forgot-password",
+  "/",
+  "/profile",
+  "/pocket",
+];
 
 const Main = ({
   children,
@@ -8,8 +21,19 @@ const Main = ({
   children: React.ReactNode;
   className?: string;
 }>) => {
+  const pathname = usePathname();
+
+  const shouldAnimate = includedPaths.includes(pathname);
+
   return (
-    <main className={cn("flex-grow p-4 pb-24", className)}>{children}</main>
+    <motion.main
+      className={cn("flex-grow p-4 pb-24", className)}
+      initial={{ opacity: shouldAnimate ? 0 : 1 }}
+      animate={{ opacity: shouldAnimate ? 1 : 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.main>
   );
 };
 
