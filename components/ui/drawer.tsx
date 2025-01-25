8f9789aa6838b6f6c01cb6a1c046b26ed50272d0
4Fort <dexterfortbsilva@gmail.com>
2025-01-25 11:37:07 +0800

    Remove undefined aria-describedby attribute from DrawerContent component

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -59,6 +59,7 @@ const DrawerContent = React.forwardRef<
         "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className
       )}
+      aria-describedby={undefined}
       {...props}
     >
       <div
